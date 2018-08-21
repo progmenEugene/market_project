ad8d01bc539d92fc88402f52d49faa02a0393c43
Jojopanda <ecompx@gmail.com>
2018-08-21 11:23:03 +0300

    Created models, views

---
+++ shop/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+#Модель категории
+class Category(models.Model):
+	name = models.CharField(max_length=200, db_index=True, verbose_name = 'Название категории')
+	slug = models.SlugField(max_length=150, db_index=True, unique = True)
+
+
+	class Meta:
+		ordering = ['name']
+		verbose_name = "Категория"
+		verbose_name_plural = "Категории"
+
+	def __str__(self):
+		return self.name
+
+#Модель продукта
+class Product(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+	name = models.CharField(max_length=300, db_index=True, verbose_name="Название товара")
+	slug = models.SlugField(max_length=250, db_index=True, unique=True, verbose_name='Метка')
+	image = models.ImageField(upload_to="images/", verbose_name="Изображение товара")
+	image_2 = models.ImageField(upload_to="images/")
+	image_3 = models.ImageField(upload_to="images/")
+	image_4 = models.ImageField(upload_to="images/")
+	description = models.TextField(verbose_name="Описание товара")
+	price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена товара')
+	available = models.BooleanField(default=True, verbose_name='Доступность')
+	created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+	updated = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+
+
+
+	class Meta:
+		verbose_name_plural = 'Товар'
+		ordering = ['name']
+		index_together = [
+		['id', 'slug']
+		]
+
+	def __str__(self):
+		return self.name
+
